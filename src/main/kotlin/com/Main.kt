ba2fb64643b80b881af6dc066fc568f63c4091f4
Mungwani <gwcowani@kangwon.ac.kr>
2026-07-15 11:54:41 +0900

    명언 앱 2단계 - 등록 기능

--- src/main/kotlin/com/Main.kt
+++ src/main/kotlin/com/Main.kt
@@ -6,10 +6,15 @@ fun main() {
     while (true){
         print("명령) ")
 
-        val input = readlnOrNull()?.trim()
+        val input = readlnOrNull()!!.trim()
 
         if(input == "종료"){
             break
+        } else if (input == "등록"){
+            print("명언 : ")
+            val content = readlnOrNull()!!.trim()
+            print("작가 : ")
+            val author = readlnOrNull()!!.trim()
         }
     }
 }
